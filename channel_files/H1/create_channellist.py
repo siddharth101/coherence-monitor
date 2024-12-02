e6790bc703bd20c279bd7b02650bce6e6b7736c0
Siddharth Soni <siddharth.soni@ldas-pcdev5.ligo.caltech.edu>
2024-12-02 11:05:36 -0800

    Added new safe channel files for L1 and H1

---
+++ channel_files/H1/create_channellist.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+filename = "H1-O4-standard.ini"
+
+
+with open(filename, "r") as file:
+    lines = file.readlines()
+    
+safe_lines = [line.strip() for line in lines if 'H1:' in line and ' safe' in line]
+unsafe_lines = [line.strip() for line in lines if 'H1:' in line and 'unsafe' in line]
+
+data_safe = []
+for line in safe_lines:
+    row = line.split()
+    data_safe.append(row)
+    
+data_unsafe = []
+for line in unsafe_lines:
+    row = line.split()
+    data_unsafe.append(row)
+    
+    
+df_safe = pd.DataFrame(data_safe, columns=['channel', 'sample_rate', 'safety', 'cleanliness'])
+df_unsafe = pd.DataFrame(data_unsafe, columns=['channel', 'sample_rate', 'safety', 'cleanliness'])
+
+df_safe['channel'].to_csv('H1_safe_channels.csv', index=None)
+df_unsafe['channel'].to_csv('H1_unsafe_channels.csv', index=None)
