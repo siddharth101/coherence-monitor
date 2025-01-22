779f3e84fc4b4e9af08cae634e9820641fc7fbe8
Siddharth Soni <siddharth.soni@ldas-pcdev6.ligo.caltech.edu>
2025-01-22 13:44:43 -0800

    Changes in unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -1,23 +1,26 @@
 import os
-import pytest
-import unittest
-import pandas as pd
 import glob
+import pandas as pd
+import unittest
+from pandas.testing import assert_frame_equal
 from utils import get_observing_segs, combine_data_files
 from gwpy.segments import Segment, SegmentList
-from pandas.testing import assert_frame_equal
 
 print("Imports done")
-segsl = SegmentList([Segment(1421395218.0, 1421400618.0)]) 
+
+# Define segments and time range
+segsl = SegmentList([Segment(1421395218.0, 1421400618.0)])
 tstart = '2025-01-20 08:00:00'
 tend = '2025-01-20 09:30:00'
-#segl_ = get_observing_segs(t1=tstart, t2=tend, ifo='L1')
+# segl_ = get_observing_segs(t1=tstart, t2=tend, ifo='L1')
 
-
+# Prepare folder path and load data
 base_path = '/home/siddharth.soni/public_html/coherence_monitor'
 folder_path = os.path.join(base_path, 'L1/2025-01-05/data/1420070718/')
 files_ = glob.glob(folder_path + "*.csv")
 nfiles = len(files_)
+
+# Combine CSV files into a single DataFrame
 li = []
 frame = pd.DataFrame()
 for i in range(nfiles):
@@ -28,24 +31,30 @@ for i in range(nfiles):
     df.value = df.value.round(2)
     df["channel"] = channame
     if len(df) > 0:
-            li.append(df)
-    frame = pd.concat(li, axis=0, ignore_index=True)
+        li.append(df)
+frame = pd.concat(li, axis=0, ignore_index=True)
 
+# Combine using utility function
 fr = combine_data_files(folder_path)
 print("Data prepared")
+
+
+# Utility function for comparing DataFrames
 def assert_dataframes_equal(test_case, df1, df2):
     try:
         assert_frame_equal(df1, df2)
     except AssertionError as e:
         test_case.fail(f"DataFrames are not equal: {e}")
 
-class testutils(unittest.TestCase):
-#    def test_observing_segs(self):
- #       self.assertEqual(segsl, segl_)
+
+# Unit test class
+class TestUtils(unittest.TestCase):
+    # Uncomment and implement this test if needed
+    # def test_observing_segs(self):
+    #     self.assertEqual(segsl, segl_)
 
     def test_combine_files(self):
         assert_dataframes_equal(self, frame, fr)
-
 
 
 if __name__ == '__main__':
