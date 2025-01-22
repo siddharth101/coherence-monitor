fd86a465d182ffcb24e7c093b0a0a8c5f4c25cd3
Siddharth Soni <siddharth.soni@ldas-pcdev6.ligo.caltech.edu>
2025-01-22 10:06:16 -0800

    Added some unit tests

---
+++ unit_tests.py
@@ -0,0 +1,49 @@
+import pytest
+import unittest
+import pandas as pd
+import glob
+from utils import get_observing_segs, combine_data_files
+from gwpy.segments import Segment, SegmentList
+from pandas.testing import assert_frame_equal
+
+print("Imports done")
+segsl = SegmentList([Segment(1421395218.0, 1421400618.0)]) 
+segl_ = get_observing_segs(t1='2025-01-20 08:00:00', t2='2025-01-20 09:30:00', ifo='L1')
+
+
+
+folder_path = '/home/siddharth.soni/public_html/coherence_monitor/L1/2025-01-05/data/1420070718/'
+files_ = glob.glob(folder_path + "*.csv")
+nfiles = len(files_)
+li = []
+for i in range(nfiles):
+    file = files_[i]
+    channame = file.split(".csv")[-2].split("/")[-1]
+    df = pd.read_csv(file, names=["freq", "value"])
+    df.freq = df.freq.round(1)
+    df.value = df.value.round(2)
+    df["channel"] = channame
+    if len(df) > 0:
+            li.append(df)
+    frame = pd.concat(li, axis=0, ignore_index=True)
+
+fr = combine_data_files('/home/siddharth.soni/public_html/coherence_monitor/L1/2025-01-05/data/1420070718/')
+print("Data prepared")
+def assert_dataframes_equal(test_case, df1, df2):
+    try:
+        assert_frame_equal(df1, df2)
+    except AssertionError as e:
+        test_case.fail(f"DataFrames are not equal: {e}")
+
+class testutils(unittest.TestCase):
+    def test_observing_segs(self):
+        self.assertEqual(segsl, segl_)
+
+    def test_combine_files(self):
+        assert_dataframes_equal(self, frame, fr)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
